8942287215b7498237763ec57d5e0ab3b649399d
Sahiln Dua <sahildua899@gmail.com>
2023-07-09 19:05:58 +0530

    testing-link

--- src/model/users.model.js
+++ src/model/users.model.js
@@ -27,7 +27,7 @@ async function findUserById(userId) {
 
 async function sendVerificationMail(userData, token) {
     if(userData.email) {
-        let link = `${process.env.PORT}/authenticate/${token}`
+        let link = `/authenticate/${token}`
     let mailDetails = {
         from: 'sahildua899@gmail.com',
         to: userData.email,
@@ -46,7 +46,7 @@ async function sendVerificationMail(userData, token) {
         const foundUser = await findUserById({_id:userData.id});
         if(foundUser) {
             const newtoken = jwt.sign({phone:foundUser.phone,email:foundUser.email},'himynameissahilduafullstackwebdeveloper', {expiresIn:"2h"});
-            let link = `${process.env.PORT}/authenticate/${newtoken}`
+            let link = `/authenticate/${newtoken}`
             let mailDetails = {
                 from: 'sahildua899@gmail.com',
                 to: foundUser.email,
